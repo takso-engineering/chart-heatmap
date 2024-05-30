c23ccd58c2d55a8070e388e077224ff6653d0438
WijekoonSR <supunrandima10@gmail.com>
2024-05-30 11:40:21 +0530

    added legends from param

--- src/App.js
+++ src/App.js
@@ -8,57 +8,21 @@ export default function App() {
     const dataParam = searchParams.get('data');
     console.log("🚀 ~ App ~ dataParam:", dataParam)
 
-
-    // const _sampleData = JSON.parse(dataParamSample)
-
-    // const data = _sampleData;
     const data = JSON.parse(dataParam);
-    console.log("🚀 ~ App ~ data:", data)
+    let  legends = get(data, 'legends', [])
 
+    if (legends && legends.length > 0) {
 
+        const _totalParticipants = legends.find(item => item.key === 'totalParticipants').value
+        const _totalRespondents = legends.find(item => item.key === 'totalRespondents').value
 
-    let _legends = [
-        {
-            key: "activityCount",
-            label: 'Activity Count',
-            value: 10
-        },
-        {
-            key: "totalParticipants",
-            label: 'Total Participants',
-            value:20
-        },
-        {
-            key: "totalRespondents",
-            label: 'Total Respondents',
-            value: 50
-        },
-
-    ]
-
-    if (_legends && _legends.length > 0) {
-
-        const _totalParticipants = _legends.find(item => item.key === 'totalParticipants').value
-        const _totalRespondents = _legends.find(item => item.key === 'totalRespondents').value
-
-        _legends = [..._legends, {
+        legends = [...legends, {
             key: "activityCount",
             label: 'Respondent Percentage',
-            value: `${(Number(_totalRespondents)/ Number(_totalParticipants)) *100}%`
-         }]
+            value: `${(Number(_totalRespondents) / Number(_totalParticipants)) * 100}%`
+        }]
     }
-
-    const _dataset = {
-        activityCount: get(data, 'activityCount', 0),
-        totalParticipants: get(data, 'totalParticipants', 0),
-        totalRespondents: get(data, 'totalRespondents', 0),
-        results: get(data, 'results', []),
-        evalCount: get(data, 'evalCount', 0),
-
-    };
-
-
-
+ 
     return (
         <div style={{ border: '1px solid #e0e0e0', }}>
             <div
@@ -71,14 +35,14 @@ export default function App() {
                     padding: '0px 20px'
                 }}
             >
-                {_legends.map(column => (
+                {legends.map(column => (
                     <div key={column.key} style={{ flex: 1, padding: '10px', textAlign: 'center' }}>
                         {`${column.label} : ${column.value}`}
                     </div>
                 ))}
             </div>
             <div style={{ padding: '20px', }}>
-                <HeatMapComponent participantsChange={_dataset} />
+                <HeatMapComponent participantsChange={{ results: get(data, 'results', []),}} />
             </div>
         </div>
 
